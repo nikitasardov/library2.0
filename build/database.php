5079b04047f01f4d6b6ffb4541f4959bda13f783
Nikita Sardov <nsardov@gmail.com>
2016-06-29 00:29:01 +0400

    from v1.0 to 2.0

--- build/database.php
+++ build/database.php
@@ -1,7 +1,7 @@
 <?php
     define('MYSQL_SERVER', 'localhost');
     define('MYSQL_USER', 'root');
-    define('MYSQL_PASSWORD', '');
+    define('MYSQL_PASSWORD', 'root');
     define('MYSQL_DB', 'library');
 
 function db_connect() {
